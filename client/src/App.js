0c7f52b251727c4eb28b17797defc781b4b0135f
James McDonald <james_mcd98@hotmail.com>
2020-03-25 14:59:01 -0400

    Added new node module for calender

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import GraphDisplay from "./components/graphDisplay";
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <GraphDisplay />
-    </div>
+    <CalendarHeatmap
+      startDate={new Date("2020-01-01")}
+      endDate={new Date("2020-12-01")}
+      values={[
+        { date: "2016-01-01" },
+        { date: "2016-01-22" },
+        { date: "2016-01-30" }
+        // ...and so on
+      ]}
+    />
   );
 }
 
