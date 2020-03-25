928f00ecf19ed2a78555e7b6366c9bced9f4614f
James McDonald <james_mcd98@hotmail.com>
2020-03-25 15:37:38 -0400

    Added dynamic date range and methods for data

--- client/src/components/GraphModule.js
+++ client/src/components/GraphModule.js
@@ -3,16 +3,28 @@ import CalendarHeatmap from "react-calendar-heatmap";
 import "react-calendar-heatmap/dist/styles.css";
 
 export default class GraphDisplay extends React.Component {
+  getStartDate() {
+    var d = new Date();
+    //probably will modify to smaller increments of like 4 - 6 months? or grow as we go
+    d.setFullYear(d.getFullYear() - 1);
+    return d;
+  }
+
+  getData() {
+    var data = [
+      { date: "2020-01-04", count: 4 },
+      { date: "2020-01-22", count: 1 },
+      { date: "2020-01-30", count: 2 }
+    ];
+    return data;
+  }
+
   render() {
     return (
       <CalendarHeatmap
-        startDate={new Date("2020-01-01")}
-        endDate={new Date("2020-12-01")}
-        values={[
-          { date: "2020-01-04", count: 4 },
-          { date: "2020-01-22", count: 1 },
-          { date: "2020-01-30", count: 2 }
-        ]}
+        startDate={this.getStartDate()}
+        endDate={Date.now()}
+        values={this.getData()}
         classForValue={value => {
           if (!value) {
             return "color-empty";
