504d9f7e486eeb0a5baf7c33df1cfb425c79692f
James McDonald <james_mcd98@hotmail.com>
2020-05-14 20:38:15 -0400

    initialized postgress connection

--- server/server.js
+++ server/server.js
@@ -4,7 +4,18 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 const passport = require("passport");
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const Client = require('pg').Client;
+const client = new Client({
+    user: "postgres",
+    password: process.env.DB_PASSWORD,
+    host:"localhost",
+    port: 5432,
+    database: process.env.DB_NAME
+})
 
 app.use(
     bodyParser.urlencoded({
@@ -13,8 +24,18 @@ app.use(
 );
 app.use(bodyParser.json())
 
-//init database once made
+connect()
 
+async function connect(){
+    try{
+        await client.connect();
+        console.log("connected to postgres")
+    }
+    catch(e){
+        console.error(`failed to connect ${e}`)
+    }
+}
+//init database once made
 app.use(passport.initialize());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
